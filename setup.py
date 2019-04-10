a3279dd1bcd1abde091b8ceaab1a553edac8c02f
Greg Reinbach <greg@reinbach.com>
2019-04-10 06:27:20 -0400

    Standardize setup.py file

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ import ast
 import codecs
 import os.path
 import re
+import subprocess
 import sys
 from codecs import open
-from setuptools import setup, find_packages
-from setuptools.command.install import install
 from distutils import log
+from distutils.errors import DistutilsError
+
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+from setuptools.command.sdist import sdist as BaseSDistCommand
 
 ROOT = os.path.realpath(os.path.dirname(__file__))
 init = os.path.join(ROOT, 'src', 'unicef_notification', '__init__.py')
@@ -30,11 +34,32 @@ def read(*files):
     return "\n".join(filter(lambda l:not l.startswith('-'), content))
 
 
-class UNICEFNotificationInstall(install):
+def check(cmd, filename):
+    out = subprocess.run(cmd, stdout=subprocess.PIPE)
+    f = os.path.join('src', 'requirements', filename)
+    reqs = codecs.open(os.path.join(ROOT, f), 'r').readlines()
+    existing = {re.split("(==|>=|<=>|<|)", name[:-1])[0] for name in reqs}
+    declared = {
+        re.split("(==|>=|<=>|<|)", name)[0]
+        for name in out.stdout.decode('utf8').split("\n")
+        if name and not name.startswith('-')
+    }
+
+    if existing != declared:
+        msg = """Requirements file not updated.
+Run 'make requiremets'
+""".format(' '.join(cmd), f)
+        raise DistutilsError(msg)
+
+
+class SDistCommand(BaseSDistCommand):
     def run(self):
-        ret = super().run()
-        log.info("This product needs pipenv for a proper installation")
-        return ret
+        checks = {'install.pip': ['pipenv', 'lock', '--requirements'],
+                  'testing.pip': ['pipenv', 'lock', '-d', '--requirements']}
+
+        for filename, cmd in checks.items():
+            check(cmd, filename)
+        super().run()
 
 
 class VerifyTagVersion(install):
@@ -50,30 +75,32 @@ class VerifyTagVersion(install):
             sys.exit(info)
 
 
-setup(name=NAME,
-      version=VERSION,
-      url='https://github.com/unicef/unicef-notification',
-      author='UNICEF',
-      author_email='dev@unicef.org',
-      license="Apache 2 License",
-      description='Django package that handles sending of notifications',
-      long_description=codecs.open('README.md').read(),
-      package_dir={'': 'src'},
-      packages=find_packages(where='src'),
-      include_package_data=True,
-      install_requires=read('install.pip'),
-      extras_require={
-          'test': read('install.pip', 'testing.pip'),
-      },
-      platforms=['any'],
-      classifiers=[
-          'Environment :: Web Environment',
-          'Programming Language :: Python :: 3.6',
-          'Framework :: Django',
-          'Intended Audience :: Developers'],
-      scripts=[],
-      cmdclass={
-          "install": UNICEFNotificationInstall,
-          "verify": VerifyTagVersion,
-      }
+setup(
+    name=NAME,
+    version=VERSION,
+    url='https://github.com/unicef/unicef-notification',
+    author='UNICEF',
+    author_email='dev@unicef.org',
+    license="Apache 2 License",
+    description='Django package that handles sending of notifications',
+    long_description=codecs.open('README.md').read(),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    install_requires=read('install.pip'),
+    extras_require={
+        'test': read('install.pip', 'testing.pip'),
+    },
+    platforms=['any'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+    ],
+    scripts=[],
+    cmdclass={
+        "install": UNICEFNotificationInstall,
+        "verify": VerifyTagVersion,
+    }
 )
